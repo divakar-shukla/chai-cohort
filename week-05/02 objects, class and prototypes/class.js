2419a7d329b1e05d8e52515b773eb114824b4c4a
divakar shukla <divakarshuklapm@gmail.com>
2025-02-20 17:03:41 +0530

    complete array challenges

---
+++ week-05/02 objects, class and prototypes/class.js
@@ -0,0 +1,18 @@
+class Person {
+  constructor(fname, lname) {
+    this.fname = fname;
+    this.lname = lname;
+
+    this.isActive = false;
+
+    console.log(this.getFullname());
+  }
+
+  getFullname() {
+    return `${this.fname} ${this.lname}`;
+  }
+}
+
+const p1 = new Person('Piyush', 'Garg');
+const p2 = new Person('Akash', 'Kadlag');
+console.log(p1);
